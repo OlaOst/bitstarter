6cb945c085eb9bff7a13f99441e8250f46946402
OlaOst <olaa81@gmail.com>
2013-07-15 22:47:52 +0000

    Autoreloading index.html

--- web.js
+++ web.js
@@ -5,11 +5,29 @@ var app = express.createServer(express.logger());
 
 var page = fs.readFileSync('index.html').toString();
 
+// reload index.html when its content changes
+var sameChange = false;
+fs.watch('index.html', function (event, filename) {
+  if (event === 'change' && filename && filename === 'index.html') {
+
+    // saving index.html triggers 3 changes, but we only want to reload once
+    if (!sameChange) {
+      setTimeout(function () { 
+        console.log("Reloading " + filename);
+        page = fs.readFileSync('index.html').toString();
+        sameChange = false;
+      }, 250);
+
+      sameChange = true;
+    }
+  }
+});
+
 app.get('/', function(request, response) {
   response.send(page);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
